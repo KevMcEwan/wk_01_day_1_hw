fd72ea4e1205af1b426936626cc202c34c25899d
KevMcEwan <kev_mcewan@hotmail.com>
2018-10-22 16:08:10 +0100

    get players test completed

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -10,4 +10,10 @@ class TestTeam < MiniTest::Test
     assert_equal("Celtic",  team1.team_name)
   end
 
+  def test_players
+      team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
+      team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+    assert_equal([ "Klos", "Albertz", "Mols"],  team2.players)
+  end
+
 end
