43d220f5d343f7c053fee2288a682e3b0e7f9795
KevMcEwan <kev_mcewan@hotmail.com>
2018-10-22 16:24:33 +0100

    attr reader and attr accessor added; tests re-run; change coach and change players tests added and run

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -16,4 +16,25 @@ class TestTeam < MiniTest::Test
     assert_equal([ "Klos", "Albertz", "Mols"],  team2.players)
   end
 
+  def test_coach
+      team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
+      team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+    assert_equal("O'Neill",  team1.coach)
+  end
+
+  def test_change_coach
+    team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
+    team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+    team1.coach = "Strachan"
+  assert_equal("Strachan",  team1.coach)
+  end
+
+  def test_add_player
+    team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
+    team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+    team2.players << "Gio"
+    assert_equal([ "Klos", "Albertz", "Mols", "Gio"], team2.players)
+  end
+
+
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,6 +1,11 @@
 
 class Team
 
+attr_reader :team_name
+attr_writer
+attr_accessor :coach, :players
+
+
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
@@ -8,17 +13,29 @@ class Team
   end
 
 
-
-  def team_name
-    return @team_name
-  end
-
-  def players
-    return @players
-  end
-
-  def coach
-    return @coach
-  end
+  #
+  # def team_name
+  #   return @team_name
+  # end
+  #
+  # def players
+  #   return @players
+  # end
+  #
+  # def coach
+  #   return @coach
+  # end
+  #
+  # def change_team_name(set_new_team_name)
+  #   @team_name = set_new_team_name
+  # end
+  #
+  # def change_players(set_new_players)
+  #   @players = set_new_players
+  # end
+  #
+  # def change_coach(set_new_coach)
+  #   @coach = set_new_coach
+  # end
 
 end
