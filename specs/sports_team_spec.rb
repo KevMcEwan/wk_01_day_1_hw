bfab7fc54dcc35157e4f9768bdeed17f452e6ade
KevMcEwan <kev_mcewan@hotmail.com>
2018-10-22 17:09:41 +0100

    added and tested function for adding points for a win

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -5,36 +5,48 @@ require_relative('../sports_team')
 class TestTeam < MiniTest::Test
 
   def test_team_name
-      team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
-      team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+      team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill", 0)
+      team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat", 0)
     assert_equal("Celtic",  team1.team_name)
   end
 
   def test_players
-      team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
-      team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+    team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill", 0)
+    team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat", 0)
     assert_equal([ "Klos", "Albertz", "Mols"],  team2.players)
   end
 
   def test_coach
-      team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
-      team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+    team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill", 0)
+    team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat", 0)
     assert_equal("O'Neill",  team1.coach)
   end
 
   def test_change_coach
-    team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
-    team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+    team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill", 0)
+    team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat", 0)
     team1.coach = "Strachan"
   assert_equal("Strachan",  team1.coach)
   end
 
   def test_add_player
-    team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
-    team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+    team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill", 0)
+    team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat", 0)
     team2.players << "Gio"
     assert_equal([ "Klos", "Albertz", "Mols", "Gio"], team2.players)
   end
 
+  # def test_check_player
+  #   team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill", 0)
+  #   team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat", 0)
+  #   assert_equal("present", team1.check_player("Larsson"))
+  # end
+
+  def test_add_points_for_a_win
+    team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill", 0)
+    team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat", 0)
+    team1.points
+    assert_equal(3, team1.add_points_for_win("win"))
+  end
 
 end
--- sports_team.rb
+++ sports_team.rb
@@ -3,14 +3,35 @@ class Team
 
 attr_reader :team_name
 attr_writer
-attr_accessor :coach, :players
+attr_accessor :coach, :players, :points
 
 
-  def initialize(team_name, players, coach)
+  def initialize(team_name, players, coach, points)
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = points
   end
+
+
+  # def check_player(team, player)
+  #   if team1.players
+  #   end
+  # end
+
+  def add_points_for_win(result)
+    point = 3 if result == "win"
+    point = 1 if result == "draw"
+    point = 0 if result == "loss"
+    @points =+ point
+  end
+
+
+    # def pay_monthly_fee
+    #   monthly_fee = 50 if @type == "personal"
+    #   monthly_fee = 100 if @type == "business"
+    #   @balance -= monthly_fee
+    # end
 
 
   #
