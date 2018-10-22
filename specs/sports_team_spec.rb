b44dd02f0f80eb90876246b4123cefb28332c9d6
KevMcEwan <kev_mcewan@hotmail.com>
2018-10-22 16:05:28 +0100

    team files set up and team name test completed

---
+++ specs/sports_team_spec.rb
@@ -0,0 +1,13 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../sports_team')
+
+class TestTeam < MiniTest::Test
+
+  def test_team_name
+      team1 = Team.new("Celtic", [ "Larsson", "Sutton", "Petrov"], "O'Neill")
+      team2 = Team.new("OldCo", [ "Klos", "Albertz", "Mols"], "Advocaat")
+    assert_equal("Celtic",  team1.team_name)
+  end
+
+end
